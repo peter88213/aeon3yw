e99643a1ef00af3e0968976259dc3f7af48b642c
peter88213 <peter.88213@t-online.de>
2021-12-20 15:38:17 +0100

    #9 Prepare a "JSON only" development version.
    - Change the converter class.
    - Convert event timestamp.
    - Abandon the Aeon2 based chapter structure.
    - Build a chapter structure matching the "Murder on the Orient Express"
    example.
    
    TODO:
    Analyze JSON[definitions][types][byId] and find out the project specific
    type IDs.

--- src/pywaeon3/json_timeline3.py
+++ src/pywaeon3/json_timeline3.py
@@ -5,9 +5,9 @@ For further information see https://github.com/peter88213/aeon3yw
 Published under the MIT License (https://opensource.org/licenses/mit-license.php)
 """
 import os
-import sys
 import json
 from datetime import datetime
+from datetime import timedelta
 
 from pywriter.model.novel import Novel
 from pywriter.model.scene import Scene
@@ -43,7 +43,7 @@ class JsonTimeline3(Novel):
     TYPE_EVENT = 'defaultEvent'
     TYPE_CHARACTER = 'defaultPerson'
     TYPE_LOCATION = 'defaultLocation'
-    TYPE_NARRATIVE = 'narrativePart'
+    TYPE_NARRATIVE = 'defaultNarrative'
 
     # JSON[definitions][types][byId][<uid>]
 
@@ -51,6 +51,9 @@ class JsonTimeline3(Novel):
 
     # Events assigned to the "narrative" become
     # regular scenes, the others become Notes scenes.
+
+    DATE_LIMIT = (datetime(100, 1, 1) - datetime.min).total_seconds()
+    # Dates before 100-01-01 can not be displayed properly in yWriter
 
     def read(self):
         """Extract the JSON part of the Aeon Timeline 3 file located at filePath, 
@@ -90,14 +93,51 @@ class JsonTimeline3(Novel):
         crIdsByGuid = {}
         lcIdsByGuid = {}
         itIdsByGuid = {}
+        scIdsByGuid = {}
+        chIdsByGuid = {}
         characterCount = 0
         locationCount = 0
         itemCount = 0
+        eventCount = 0
+        chapterCount = 0
 
         for uid in jsonData['data']['items']['byId']:
             dataItem = jsonData['data']['items']['byId'][uid]
 
-            if dataItem[self.ITEM_TYPE] == self.TYPE_CHARACTER:
+            if dataItem[self.ITEM_TYPE] == self.TYPE_EVENT:
+                #--- Create scenes.
+
+                eventCount += 1
+                scId = str(eventCount)
+                scIdsByGuid[uid] = scId
+                self.scenes[scId] = Scene()
+                self.scenes[scId].status = 1
+                # Set scene status = "Outline"
+                self.scenes[scId].title = dataItem[self.ITEM_LABEL]
+                self.scenes[scId].desc = dataItem[self.ITEM_DESCRIPTION]
+                timestamp = dataItem[self.ITEM_START_DATE]['timestamp']
+
+                if timestamp is not None and timestamp >= self.DATE_LIMIT:
+                    # Restrict date/time calculation to dates within yWriter's range
+
+                    sceneStart = datetime.min + timedelta(seconds=timestamp)
+                    startDateTime = sceneStart.isoformat().split('T')
+                    self.scenes[scId].date = startDateTime[0]
+                    self.scenes[scId].time = startDateTime[1]
+
+            elif dataItem[self.ITEM_TYPE] == self.TYPE_NARRATIVE:
+                #--- Create chapters.
+
+                chapterCount += 1
+                chId = str(chapterCount)
+                chIdsByGuid[uid] = chId
+                self.chapters[chId] = Chapter()
+                self.chapters[chId].title = dataItem[self.ITEM_LABEL]
+                self.chapters[chId].desc = dataItem[self.ITEM_DESCRIPTION]
+
+            elif dataItem[self.ITEM_TYPE] == self.TYPE_CHARACTER:
+                #--- Create characters.
+
                 characterCount += 1
                 crId = str(characterCount)
                 crIdsByGuid[uid] = crId
@@ -107,6 +147,9 @@ class JsonTimeline3(Novel):
                 self.srtCharacters.append(crId)
 
             elif dataItem[self.ITEM_TYPE] == self.TYPE_LOCATION:
+
+                #--- Create locations.
+
                 locationCount += 1
                 lcId = str(locationCount)
                 lcIdsByGuid[uid] = lcId
@@ -117,6 +160,9 @@ class JsonTimeline3(Novel):
 
             '''
             elif dataItem[self.ITEM_TYPE] == self.TYPE_ITEM:
+
+                #--- Create items.
+
                 itemCount += 1
                 itId = str(itemCount)
                 itIdsByGuid[uid] = itId
@@ -126,56 +172,36 @@ class JsonTimeline3(Novel):
                 self.srtItems.append(itId)
             '''
 
-        #--- Create scenes.
+        #--- Build the narrative structure.
 
-        eventCount = 0
-        scIdsByDate = {}
+        for narrative0 in jsonData['data']['narrative']['children']:
 
-        for uid in jsonData['data']['items']['byId']:
-            dataItem = jsonData['data']['items']['byId'][uid]
+            if narrative0['id'] in chIdsByGuid:
+                self.srtChapters.append(chIdsByGuid[narrative0['id']])
 
-            if dataItem[self.ITEM_TYPE] == self.TYPE_EVENT:
-                eventCount += 1
-                scId = str(eventCount)
-                self.scenes[scId] = Scene()
-                #self.scenes[scId].isNotesScene = noScene
-                self.scenes[scId].title = dataItem[self.ITEM_LABEL]
-                self.scenes[scId].desc = dataItem[self.ITEM_DESCRIPTION]
-                timestamp = dataItem[self.ITEM_START_DATE]['timestamp']
+            for narrative1 in narrative0['children']:
 
-                if timestamp is None:
-                    timestamp = 0
+                if narrative1['id'] in chIdsByGuid:
+                    self.srtChapters.append(chIdsByGuid[narrative1['id']])
+                    self.chapters[chIdsByGuid[narrative0['id']]].chLevel = 1
 
-                if not timestamp in scIdsByDate:
-                    scIdsByDate[timestamp] = []
+                    for narrative2 in narrative1['children']:
 
-                scIdsByDate[timestamp].append(scId)
+                        if narrative2['id'] in scIdsByGuid:
+                            self.chapters[chIdsByGuid[narrative1['id']]].srtScenes.append(
+                                scIdsByGuid[narrative2['id']])
+                            self.chapters[chIdsByGuid[narrative1['id']]].chLevel = 0
 
-        #--- Sort scenes by date/time and place them in chapters.
+                elif narrative1['id'] in scIdsByGuid:
+                    self.chapters[chIdsByGuid[narrative1['id']]].srtScenes.append(scIdsByGuid[narrative1['id']])
+                    self.chapters[chIdsByGuid[narrative0['id']]].chLevel = 0
 
-        chIdNarrative = '1'
-        chIdBackground = '2'
+        # Create a dummy chapter, if there is no other structure.
 
-        self.chapters[chIdNarrative] = Chapter()
-        self.chapters[chIdNarrative].title = 'Chapter 1'
-        self.chapters[chIdNarrative].chType = 0
-        self.srtChapters.append(chIdNarrative)
-
-        self.chapters[chIdBackground] = Chapter()
-        self.chapters[chIdBackground].title = 'Background'
-        self.chapters[chIdBackground].chType = 1
-        self.srtChapters.append(chIdBackground)
-
-        srtScenes = sorted(scIdsByDate.items())
-
-        for date, scList in srtScenes:
-
-            for scId in scList:
-
-                if self.scenes[scId].isNotesScene:
-                    self.chapters[chIdBackground].srtScenes.append(scId)
-
-                else:
-                    self.chapters[chIdNarrative].srtScenes.append(scId)
+        if self.chapters == {}:
+            self.chapters['1'] = Chapter()
+            self.chapters['1'].title = 'Chapter 1'
+            self.chapters['1'].chType = 0
+            self.srtChapters.append('1')
 
         return 'SUCCESS: Data read from "' + os.path.normpath(self.filePath) + '".'
