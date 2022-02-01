2507c617f94f903ab112ddbbe6026863739644f0
peter88213 <peter.88213@t-online.de>
2022-02-01 15:18:36 +0100

    Update build system.

--- src/build_aeon3yw.py
+++ src/build_aeon3yw.py
@@ -10,6 +10,8 @@ For further information see https://github.com/peter88213/aeon3yw
 Published under the MIT License (https://opensource.org/licenses/mit-license.php)
 """
 import os
+import sys 
+sys.path.insert(0, f'{os.getcwd()}/../../PyWriter/src')
 import inliner
 
 SRC = '../src/'
@@ -19,14 +21,6 @@ TARGET_FILE = f'{BUILD}aeon3yw.pyw'
 
 
 def main():
-    os.chdir(SRC)
-
-    try:
-        os.remove(TARGET_FILE)
-
-    except:
-        pass
-
     inliner.run(SOURCE_FILE, TARGET_FILE, 'pywaeon3', '../src/')
     inliner.run(TARGET_FILE, TARGET_FILE, 'pywriter', '../../PyWriter/src/')
     print('Done.')
