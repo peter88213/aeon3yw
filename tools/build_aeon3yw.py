dbba0a3fdb2d58bc24f1c34ea3e0081ed1f4dced
peter88213 <peter.88213@t-online.de>
2023-06-09 12:50:12 +0200

    Update docstring

--- tools/build_aeon3yw.py
+++ tools/build_aeon3yw.py
@@ -1,11 +1,9 @@
-""" Build a python script for the aeon3yw distribution.
+"""Build a Python script for the aeon3yw distribution.
         
 In order to distribute a single script without dependencies, 
 this script "inlines" all modules imported from the pywriter package.
 
-The PyWriter project (see see https://github.com/peter88213/PyWriter)
-must be located on the same directory level as the aeon3yw project. 
-
+Copyright (c) 2023 Peter Triesberger
 For further information see https://github.com/peter88213/aeon3yw
 Published under the MIT License (https://opensource.org/licenses/mit-license.php)
 """
